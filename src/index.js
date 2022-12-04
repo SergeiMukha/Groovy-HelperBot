4ae26db095e53c7ba9c29233fcbb174821973ad0
SergeiMukha <sergeimushonok@example.com>
2022-12-04 23:16:17 +0200

    typo fixed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 
 const { setUpBot } = require('./bot');
-const { pool } = require('./db')
 
 const { URL, BOT_TOKEN } = process.env;
 const PORT = process.env.PORT || 5000;
 
 (async function () {
-
+    
     console.log('Starting');
 
-    bot = setUpBot(pool);
+    bot = setUpBot(BOT_TOKEN);
 
     if (process.env.NODE_ENV == "production") {
         bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
